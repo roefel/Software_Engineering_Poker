f7625e818aadc803aba9cb93fae0ecbce1f488c8
Angeluso <angelboy550@hotmail.com>
2016-11-04 12:21:30 +0100

    added moneymodel to tableCModel

--- Software_Engineering_Poker/Software_Engineering_Poker/TableContainer/TableContainerModel.cs
+++ Software_Engineering_Poker/Software_Engineering_Poker/TableContainer/TableContainerModel.cs
@@ -8,12 +8,15 @@ namespace Software_Engineering_Poker
 {
     public class TableContainerModel
     {
+        private MoneyModel moneyModel;
         protected int mtableMoneyValue;
         protected Boolean mIsFixed;
 
         public TableContainerModel()
         {
-            mtableMoneyValue = 0;
+            moneyModel = new MoneyModel();
+            mtableMoneyValue = moneyModel.currentPlayerBalance;
+            //mtableMoneyValue = 0;
             mIsFixed = false;
         }
 
